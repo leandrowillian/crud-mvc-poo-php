9e164b88abd108e9c8f423040b5afaa5ad472380
Willian Leandro <williank10@icloud.com>
2022-08-08 23:46:59 -0300

    Criação da classe de conexão com banco de dados

--- configuration/connect.php
+++ configuration/connect.php
@@ -1,1 +1,37 @@
 <!-- Conexão com o banco de dados -->
+<?php
+
+    // Definindo as constantes
+
+    define('HOST', 'localhost');
+    define('DB', 'crud-mvc-poo');
+    define('USER', 'root');
+    define('PASSWORD', '');
+
+
+    // Criando classe de conexão com o DB
+    class Connect
+    {
+        // Criando atributo protegido para apenas essa classe ou uma que estender ela acessar
+        protected $conn;
+
+        // método mágico construtor
+        function __construct()
+        {
+            $this->connectDb();
+        }
+
+        function connectDb()
+        {
+            // Utilizando try catch para tratar erros
+            try
+            {
+                $this->conn = new PDO('mysql:host='.HOST.';dbname='.DB, USER, PASSWORD);
+            }
+            catch (PDOException $e)
+            {
+                echo "Error!! ". $e->getMessage();
+                // die();
+            }
+        }
+    }
